bc996bb78563a8692845f160ce3fe87c6b99deb5
unknown <lithos.contact@gmail.com>
2023-03-04 16:31:13 +0100

    change test name

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import request from 'supertest';
 
-describe('GET /user', () => {
+describe('GET /test', () => {
 	test('should respond with code status 200', async () => {
 		const response = await request(app).get('/test').send();
 		expect(response.status).toBe(200);
